c866e535e343d867c67c69240c287ef8c9eac8d4
mariyamkhn <mariyamkhn85@gmail.com>
2023-09-16 15:15:46 +0530

    dictionary data type

---
+++ main.py
@@ -0,0 +1,9 @@
+
+#dictionary data type
+country={"india":"delhi",
+         "australia":"canberra",
+         'belgium':'brussels',
+         'canada':'ottawa',
+         'china':'beijing',}
+print(country)
+#in dictionary data type we use curly brackets
